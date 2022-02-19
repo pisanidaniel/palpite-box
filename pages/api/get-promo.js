ea45853052575e85fd26620243d428e40759ae28
pisanidaniel <dfpisani@gmail.com>
2022-02-19 17:49:18 -0300

    adding base64 stable

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,16 +1,12 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import moment from 'moment'
+import { fromBase64  } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-const fromBase64 = value => {
-    const buff = new Buffer(value, 'base64');
-    return buff.toString('ascii');
-}
+
 
 
 export default async(req,res) => {
-    console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
     try {  
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL ,
